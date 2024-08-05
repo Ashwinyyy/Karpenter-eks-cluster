882e28a7893ab1f6f47b5258a38a9feed0f4e6bb
perumal raj <saranaws01@gmail.com>
2024-08-05 09:52:15 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,31 +11,47 @@ variable "node_group_name" {
 }
 
 variable "desired_capacity" {
-  description = "The desired number of instances in the node group"
-  type        = number
-  default     = 3
+  type    = number
+  default = 3
 }
 
 variable "max_capacity" {
-  description = "The maximum number of instances in the node group"
-  type        = number
-  default     = 2
+  type    = number
+  default = 3
 }
 
 variable "min_capacity" {
-  description = "The minimum number of instances in the node group"
-  type        = number
-  default     = 1
+  type    = number
+  default = 1
 }
 
 variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for the node group"
-  type        = string
-  default     = "saransasthaohio"  # SSH key name without .pem extension
+  type    = string
+  default = "saransasthaohio"
 }
 
 variable "subnet_ids" {
   description = "A list of subnet IDs"
   type        = list(string)
-  default     = ["subnet-07b275afa3388452d", "subnet-091ff436aaf3eefcb"]
+  default     = [
+    "subnet-07b275afa3388452d",  # tf_pub1
+    "subnet-091ff436aaf3eefcb"   # tf_pub2
+  ]
+}
+
+variable "vpc_id" {
+  description = "The VPC ID"
+  type        = string
+  default     = "vpc-00f0043ea99fca7ea"
+}
+
+variable "security_group_ids" {
+  description = "A list of security group IDs"
+  type        = list(string)
+  default     = [
+    "sg-0fda6a8acba9fe447",  # ec2_sg
+    "sg-01e65be545d8631e9",  # ansible_sg
+    "sg-06e9945ae9421c663",  # alb_sg
+    "sg-02085204110da41e2"   # bastion_sg
+  ]
 }
