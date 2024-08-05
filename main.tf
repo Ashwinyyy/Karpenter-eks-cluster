f7d9904a631d5bf6af145be0d63e79bd6d202b05
perumal raj <saranaws01@gmail.com>
2024-08-05 09:59:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,65 @@
-data "aws_iam_role" "eks_cluster_role" {
+resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-data "aws_iam_role" "eks_node_role" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  lifecycle {
+    ignore_changes = [assume_role_policy]  # Ignore changes to the assume role policy
+    prevent_destroy = true  # Prevent accidental deletion
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.eks_cluster_name
-  role_arn = data.aws_iam_role.eks_cluster_role.arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = var.subnet_ids
@@ -16,7 +67,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   }
 
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = true  # Prevent accidental deletion
   }
 
   depends_on = [
@@ -27,7 +78,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = var.node_group_name
-  node_role_arn   = data.aws_iam_role.eks_node_role.arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.subnet_ids
 
   scaling_config {
