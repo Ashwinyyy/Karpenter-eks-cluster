901f8be733b5aba9afbff9959f21da624f36baa2
perumal raj <saranaws01@gmail.com>
2024-08-05 08:49:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,34 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# IAM role for EKS Cluster
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
 # Define the EKS cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.eks_cluster_name
@@ -64,7 +92,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   }
 
   remote_access {
-    ec2_ssh_key = var.ssh_key_name  # Ensure this matches your SSH key pair name
+    ec2_ssh_key = var.ssh_key_name
   }
 
   depends_on = [
