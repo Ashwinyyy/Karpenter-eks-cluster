cc27561738439157bcc1980ac73d872118f9ac4c
perumal raj <saranaws01@gmail.com>
2024-08-05 09:52:08 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-2"  # Update this to your desired region
 }
 
-# EKS Cluster IAM Role
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
 
@@ -25,27 +24,24 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   role       = aws_iam_role.eks_cluster_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceController" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-# EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
   name     = var.eks_cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = var.subnet_ids
+    security_group_ids = var.security_group_ids
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSVPCResourceController,
   ]
 }
 
-# EKS Node IAM Role
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
 
@@ -61,6 +57,10 @@ resource "aws_iam_role" "eks_node_role" {
       }
     ]
   })
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
@@ -78,7 +78,6 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.eks_node_role.name
 }
 
-# EKS Node Group
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = var.node_group_name
@@ -92,7 +91,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   }
 
   remote_access {
-    ec2_ssh_key = var.ssh_key_name  # Ensure this matches your SSH key pair name
+    ec2_ssh_key = var.ssh_key_name
   }
 
   depends_on = [
@@ -100,8 +99,4 @@ resource "aws_eks_node_group" "eks_node_group" {
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
   ]
-}
-
-output "eks_cluster_id" {
-  value = aws_eks_cluster.eks_cluster.id
 }
