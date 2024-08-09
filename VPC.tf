8f9582bde37b16d86d83662ef9bd5ffdabecea7b
perumal raj <saranaws01@gmail.com>
2024-08-09 09:16:16 +0000

    Update VPC.tf

--- VPC.tf
+++ VPC.tf
@@ -1,23 +1,7 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.123.0.0/16"
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
-
-  enable_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+  tags = {
+    Name = "main"
   }
 }
